ceb562e830405c47c47ea17f501e117daa29387f
christianwilburn <98135173+christianwilburn@users.noreply.github.com>
2022-04-11 21:58:58 -0400

    add main and update

--- src/App.js
+++ src/App.js
@@ -1,28 +1,14 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import Directory from './components/DirectoryComponent';
+import Main from './components/MainComponent';
 import './App.css';
-import { CAMPSITES} from './shared/campsites'
+
 
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            campsites: CAMPSITES,
-            selectedCampsite: null
-        };
-    }
   render() {
       return (
           <div className="App">
-              <Navbar dark color="primary">
-              <div className="container">
-                  <NavbarBrand href="/">NuCamp</NavbarBrand>
-              </div>
-              </Navbar>
-              <Directory campsites={this.state.campsites} />
-              {/* <CampsiteInfo campsite={this.state.campsites.filter(campsite => campsite.id === this.state.selectedCampsite)[0]}/> */}
+             <Main />
           </div>
       );
   }
