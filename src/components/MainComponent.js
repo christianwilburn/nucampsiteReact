68f2718dbe8dbfd235852c3a0e2abc075717ac97
christianwilburn <98135173+christianwilburn@users.noreply.github.com>
2022-04-15 22:38:04 -0400

    Header and footer

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,45 +1,48 @@
-import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import Directory from './DirectoryComponent';
-import CampsiteInfo from './CampsiteInfoComponent';
+import React, { Component } from "react";
 
-import { CAMPSITES} from '../shared/campsites'
-
+import Directory from "./DirectoryComponent";
+import CampsiteInfo from "./CampsiteInfoComponent";
+import Header from "./HeaderComponent";
+import Footer from "./FooterComponent";
+import { CAMPSITES } from "../shared/campsites";
 
 // Main component starts
 
-
 class Main extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            campsites: CAMPSITES,
-            selectedCampsite: null
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      campsites: CAMPSITES,
+      selectedCampsite: null,
+    };
+  }
 
-    // onCampsiteSelect starts here
+  // onCampsiteSelect starts here
 
-    onCampsiteSelect(campsiteId) {
-        this.setState({ selectedCampsite: campsiteId });
-      }
+  onCampsiteSelect(campsiteId) {
+    this.setState({ selectedCampsite: campsiteId });
+  }
 
-    //   onCampsiteSelect ends here
+  //   onCampsiteSelect ends here
 
-
-    
   render() {
-      return (
-          <div>
-              <Navbar dark color="primary">
-              <div className="container">
-                  <NavbarBrand href="/">NuCamp</NavbarBrand>
-              </div>
-              </Navbar>
-              <Directory campsites={this.state.campsites} onClick={campsiteId => this.onCampsiteSelect(campsiteId)}/>
-              <CampsiteInfo campsite={this.state.campsites.filter(campsite => campsite.id === this.state.selectedCampsite)[0]} />
-          </div>
-      );
+    return (
+      <div>
+        <Header />
+        <Directory
+          campsites={this.state.campsites}
+          onClick={(campsiteId) => this.onCampsiteSelect(campsiteId)}
+        />
+        <CampsiteInfo
+          campsite={
+            this.state.campsites.filter(
+              (campsite) => campsite.id === this.state.selectedCampsite
+            )[0]
+          }
+        />
+        <Footer />
+      </div>
+    );
   }
 }
 
