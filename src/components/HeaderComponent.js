ffcf73bece0493fd0920f48e45da96db90b1ab6b
christianwilburn <98135173+christianwilburn@users.noreply.github.com>
2022-04-22 21:49:00 -0400

    uncontrolled form - login modal

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,27 +1,67 @@
 import React, { Component } from "react";
-import { Nav, NavbarToggler, Collapse, NavItem, Jumbotron, Navbar, NavbarBrand } from "reactstrap";
-import {NavLink} from 'react-router-dom';
+import {
+  Nav,
+  NavbarToggler,
+  Collapse,
+  NavItem,
+  Jumbotron,
+  Navbar,
+  NavbarBrand,
+  ModalHeader,
+  Modal,
+  ModalBody,
+  Button,
+  Form,
+  FormGroup,
+  Input,
+  Label,
+} from "reactstrap";
+import { NavLink } from "react-router-dom";
 
 class Header extends Component {
+  // constructor starts
 
   constructor(props) {
     super(props);
 
     this.toggleNav = this.toggleNav.bind(this);
+    this.toggleModal = this.toggleModal.bind(this);
+    this.handleLogin = this.handleLogin.bind(this);
+
     this.state = {
-      isNavOpen : false
+      isNavOpen: false,
+      isModalOpen: false,
     };
   }
 
   toggleNav() {
     this.setState({
-      isNavOpen : !this.state.isNavOpen
+      isNavOpen: !this.state.isNavOpen,
     });
   }
 
+  toggleModal() {
+    this.setState({
+      isModalOpen: !this.state.isModalOpen,
+    });
+  }
+
+  handleLogin(event) {
+    alert(
+      `Username: ${this.username.value} Password: ${this.password.value} Remember: ${this.remember.checked}`
+    );
+    this.toggleModal();
+
+    // below prevents page reload
+
+    event.preventDefault();
+  }
+
+  // constructor ends
+
   render() {
     return (
-      <React.Fragment>
+      <>
         <Jumbotron fluid>
           <div className="container">
             <div className="row">
@@ -32,37 +72,98 @@ class Header extends Component {
             </div>
           </div>
         </Jumbotron>
-        <Navbar dark sticky="top" expand='md'>
+
+        {/* Navbar Begins */}
+
+        <Navbar dark sticky="top" expand="md">
           <div className="container">
-            <NavbarBrand className='mr-auto' href="/"><img src='/assets/images/logo.png' height='30' width='30' alt='Nucamp Logo' /></NavbarBrand>
+            <NavbarBrand className="mr-auto" href="/">
+              <img
+                src="/assets/images/logo.png"
+                height="30"
+                width="30"
+                alt="Nucamp Logo"
+              />
+            </NavbarBrand>
             <NavbarToggler onClick={this.toggleNav} />
-            <Collapse isOpen={this.state.isNavOpen} navbar >
-            <Nav navbar>
-              <NavItem>
-                <NavLink className='nav-link' to='/home'>
-                  <i className="fa fa-home fa-lg"/>Home
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink className='nav-link' to='/directory'>
-                  <i className='fa fa-list fa-lg'/>Directory
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink className='nav-link' to='/aboutus'>
-                  <i className='fa fa-info fa-lg' />About
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink className='nav-link' to='/contactus'>
-                  <i className="fa fa-address-card fa-lg"/>Contact Us
-                </NavLink>
-              </NavItem>
-            </Nav>
+            <Collapse isOpen={this.state.isNavOpen} navbar>
+              <Nav navbar>
+                <NavItem>
+                  <NavLink className="nav-link" to="/home">
+                    <i className="fa fa-home fa-lg" />
+                    Home
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink className="nav-link" to="/directory">
+                    <i className="fa fa-list fa-lg" />
+                    Directory
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink className="nav-link" to="/aboutus">
+                    <i className="fa fa-info fa-lg" />
+                    About
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink className="nav-link" to="/contactus">
+                    <i className="fa fa-address-card fa-lg" />
+                    Contact Us
+                  </NavLink>
+                </NavItem>
+              </Nav>
+              <span className="navbar-text ml-auto">
+                <Button outline onClick={this.toggleModal}>
+                  <i className="fa fa-sign-in fa-lg" />
+                </Button>
+              </span>
             </Collapse>
           </div>
         </Navbar>
-      </React.Fragment>
+
+        {/* navbar ends */}
+
+        {/* Login Modal starts */}
+
+        <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+          <ModalHeader toggle={this.toggleModal}>Login</ModalHeader>
+          <ModalBody>
+            <Form onSubmit={this.handleLogin}>
+              <FormGroup>
+                <Label htmlFor="username">Username</Label>
+                <Input
+                  type="text"
+                  id="username"
+                  name="username"
+                  innerRef={(input) => (this.username = input)}
+                />
+              </FormGroup>
+              <FormGroup>
+                <Label htmlFor="password">Password</Label>
+                <Input
+                  type="password"
+                  id="password"
+                  name="password"
+                  innerRef={(input) => (this.password = input)}
+                />
+              </FormGroup>
+              <FormGroup check>
+                <Label check>
+                  <Input
+                    type="checkbox"
+                    name="remember"
+                    innerRef={(input) => (this.remember = input)}
+                  />
+                  Remember me
+                </Label>
+              </FormGroup>
+            </Form>
+          </ModalBody>
+        </Modal>
+
+        {/* Login Modal Ends */}
+      </>
     );
   }
 }
